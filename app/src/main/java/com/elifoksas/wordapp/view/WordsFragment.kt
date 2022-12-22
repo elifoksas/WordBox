00c423f9d758dce48ec1d7e31487399b1250eaee
elifoksas <62424342+elifoksas@users.noreply.github.com>
2022-12-23 00:29:32 +0300

    texttospeech added

--- app/src/main/java/com/elifoksas/wordapp/view/WordsFragment.kt
+++ app/src/main/java/com/elifoksas/wordapp/view/WordsFragment.kt
@@ -2,6 +2,8 @@ package com.elifoksas.wordapp.view
 
 import android.opengl.Visibility
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.speech.tts.Voice
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -17,14 +19,19 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.squareup.picasso.Picasso
+import java.util.*
+import kotlin.collections.ArrayList
 
-class WordsFragment : Fragment() {
+class WordsFragment : Fragment(), TextToSpeech.OnInitListener {
 
     private lateinit var binding: FragmentWordsBinding
 
 
     var categoryid = 0
     var categoryName = ""
+    lateinit var textToSpeech : TextToSpeech
+
+
 
 
 
@@ -65,6 +72,9 @@ class WordsFragment : Fragment() {
             }
             if (categoryid==7){
                 categoryName = "animals"
+            }
+            if (categoryid==8){
+                categoryName = "body parts"
             }
             Log.d("categoryname",categoryName.toString())
 
@@ -164,11 +174,45 @@ class WordsFragment : Fragment() {
 
     }
 
+    private fun speakOut(){
+        val text = binding.wordTxt.text.toString()
+        textToSpeech.speak(text,TextToSpeech.QUEUE_FLUSH,null,"")
+
+
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        textToSpeech = TextToSpeech(context,this)
 
+
+
+
+
+
+        binding.voiceBtn.setOnClickListener {
+
+            textToSpeech.language = Locale.ENGLISH
+
+            speakOut()
+
+        }
+
+
+
+    }
+
+    override fun onDestroy() {
+        if (textToSpeech!=null){
+            textToSpeech!!.stop()
+            textToSpeech!!.shutdown()
+        }
+        super.onDestroy()
+    }
+
+    override fun onInit(p0: Int) {
 
     }
 
